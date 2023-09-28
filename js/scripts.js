0afde5d0bad4c72ed834bd04bc5ca41a1b9effe0
iarajfares <iarajfares@gmail.com>
2023-09-28 15:02:56 -0300

    validacion js

--- js/scripts.js
+++ js/scripts.js
@@ -54,3 +54,49 @@ modal.forEach((modal)=> {
         modal.classList.remove('modal-show');
     });
 });
+
+// validaciones del formulario 
+const nameField = document.querySelector('#name_label');
+const emailField = document.querySelector('#email_label');
+const subjectField = document.querySelector('#subject_label');
+const commentsField = document.querySelector('#comments_label');
+
+const setErrors = (message, field, isError = true) => {
+    if (isError) {
+        field.classList.add('invalid');
+        field.nextElementSibling.classList.add('error');
+        field.nextElementSibling.innerHTML = message;
+    } else {
+        field.classList.remove('invalid');
+        field.nextElementSibling.classList.remove('error');
+        field.nextElementSibling.innerHTML = "";
+    }
+}
+// validar campos vacios 
+const validateEmptyField = (message, e) => {
+    const field = e.target;
+    const fieldValue = e.target.value;
+    if (fieldValue.trim().length === 0) {
+        setErrors(message, field);
+    } else {
+        setErrors("", field, false);
+    }
+}
+// validar email correcto
+const validateEmailFormat = e => {
+    const field = e.target;
+    const fieldValue = e.target.value;
+    const regex = new RegExp(/^\w+([.-_+]?\w+)@\w+([.-]?\w+)(.\w{2,10})+$/);
+    if (fieldValue.trim().length > 5 && !regex.test(fieldValue)) {
+        setErrors("Debe ingresar un email válido", field);
+    } else {
+        setErrors("", field, false);
+    }
+}
+
+nameField.addEventListener("blur", (e) => validateEmptyField("Este campo no puede estar vacío", e));
+emailField.addEventListener("blur", (e) => validateEmptyField("Debe ingresar un email válido", e));
+subjectField.addEventListener("blur", (e) => validateEmptyField("Este campo no puede estar vacío", e));
+commentsField.addEventListener("blur", (e) => validateEmptyField("Este campo no puede estar vacío", e));
+
+emailField.addEventListener("input", validateEmailFormat);
